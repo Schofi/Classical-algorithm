259811f9123c43e3f59549aeddaac441fb5b5c78
Schofi <33537727+Schofi@users.noreply.github.com>
2018-10-30 20:32:25 +0800

    Create BubbleSort.cpp

---
+++ Sorting Algorithm/BubbleSort.cpp
@@ -0,0 +1,15 @@
+template<class T>
+void Bubblesort(T a[],int n){
+	for(int i=0;i<n-1;i++){
+		for(int j=0;j<n-i-1;j++){
+			if(a[j]>a[j+1]){
+				swap(a[j],a[j+1]);
+			}
+		}
+	}
+}
+void swap(T *a,T *b){
+	int c=*a;
+	*a=*b;
+	*b=c;
+}
